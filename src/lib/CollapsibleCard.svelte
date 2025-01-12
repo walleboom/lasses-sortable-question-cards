0a729daffb94e6f06ca165220d08489e450416cd
walleboom <johannes.walleboom@gmail.com>
2025-01-12 02:07:01 +0100

    Add CollapsibleCard component

---
+++ src/lib/CollapsibleCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import collapse from "svelte-collapse";
+
+    let {
+        open = true,
+        header,
+        body,
+        ...otherProps
+    } = $props();
+</script>
+
+<div {...otherProps}>
+    {@render header()}
+    <div use:collapse={{ open }}>
+        {@render body()}
+    </div>
+</div>
